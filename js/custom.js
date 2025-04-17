d947760875c986db7be3163f434c48a4ecc6cf36
Godswill Hailsham <godswillhailsham@yahoo.com>
2025-04-17 10:02:45 +0100

    second commit

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,4 @@
-// // Get Current Year
-// function getCurrentYear() {
-//     var d = new Date();
-//     var year = d.getFullYear();
-//     document.querySelector("#displayDateYear").innerText = year;
-// }
-// getCurrentYear()
+
 
 //client section owl carousel
 $(".owl-carousel").owlCarousel({
